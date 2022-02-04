c0080de9cfb6a721a830bef846e608ed490f1b3b
Layravbf <layravilas@hotmail.com>
2022-02-03 22:09:07 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,33 @@
+public class D {
+    double D1;
+    int D2;
+
+    public D(double D1, int D2){
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    public double getD1(){
+        return D1;
+    }
+
+    public void setD1(double D1){
+        this.D1 = D1;
+    }
+
+    public int getD2(){
+        return D2;
+    }
+
+    public void setD2(int D2){
+        this.D2 = D2;
+    }
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
